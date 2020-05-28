b958cbac91ecef657e0d88496dda0bcee4d90c37
linhuchong <linhu@qq.com>
2020-05-28 17:15:25 +0800

    令狐冲提交

--- src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -7,5 +7,10 @@ package com.atguigu.jianfa.dugujiujian;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("jianfa  dugujiujian");
+        System.out.println("jianfa  dugujiujian");
+        System.out.println("jianfa  dugujiujian");
+        System.out.println("jianfa ");
+        System.out.println("jianfa  dugujiujian");
+        System.out.println("jianfa  dugujiujian");
     }
 }
